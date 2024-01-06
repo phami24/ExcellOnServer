e437831094c9e0ba89c5752cd3a8cfff36be863e
Tạ Thị Thanh Thủy <tathanhthuy17032004@gmail.com>
2024-01-06 10:43:26 +0700

    fix routing home

--- Excell-on-Services/src/app/app-routing.module.ts
+++ Excell-on-Services/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/client',
+    redirectTo: '/home',
     pathMatch: 'full',
   },
   {
