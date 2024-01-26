2b443f6d6b32e31142e4ea912961547e137b9e4c
toandv <toando2408@gmail.com>
2024-01-26 13:38:51 +0700

    emp

--- Excell-on-Services/src/app/auth/services/client.service.ts
+++ Excell-on-Services/src/app/auth/services/client.service.ts
@@ -6,7 +6,7 @@ import { IClientState } from '../../State/client/client.states';
 import * as fromClient from '../../State/client/client.actions';
 import { Banner } from 'src/app/interfaces/banner';
 
-const baseUrl = 'https://localhost:7180/api';
+const baseUrl = 'https://localhost:7260/api';
 
 @Injectable({
   providedIn: 'root',
@@ -34,7 +34,7 @@ export class ClientService {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const body = { email, password };
 
-    return this.http.post<{ result: boolean; token: string }>(`${baseUrl}/Auth/Login`, body, { headers }).pipe(
+    return this.http.post<{ result: boolean; token: string }>(`${baseUrl}/Auth/Emp-Login`, body, { headers }).pipe(
       map(response => response),
       catchError(error => {
         console.error('Login failed:', error);
