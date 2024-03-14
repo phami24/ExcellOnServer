5b19eec4447004bd3042f18dd8d7c44c41c05fe2
Minh Pham <buibinhminhpham2004@gmail.com>
2024-03-14 20:31:50 +0700

    message

---
+++ Excell-on-Services/src/app/interfaces/group.ts
@@ -0,0 +1,10 @@
+export interface Group {
+    groupId: string;
+    employeeId: number;
+    clientId: number;
+    employeeName: string;
+    customerName: string;
+    groupName: string;
+    employeeEmail: string;
+    customerEmail: string;
+}
